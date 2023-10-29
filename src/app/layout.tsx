ff002dd2c05cee38071b04836498c7b71517d58f
jasper-hyj <jasper.huang6@gmail.com>
2023-10-29 03:09:11 -0700

    Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,6 +45,21 @@ export default async function RootLayout({
 						document.getElementsByTagName("body")[0].appendChild(tag);
 					});`}
 					</Script>
+					<Script
+						strategy="lazyOnload"
+						src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+					/>
+					{/* eslint-disable-next-line @next/next/inline-script-id */}
+					<Script strategy="lazyOnload">
+						{`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+					</Script>
 				</NextAuthProvider>
 			</body>
 		</html>
